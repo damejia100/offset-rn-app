a57b3cd59bab7c6ff327dc7b78108860495931e7
devAM100 <devismejia@gmail.com>
2019-11-15 17:17:34 -0500

    splash and sign up screen completed

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignUp from './app/components/SignUp'
+// import {StackNavigator} from 'react-navigation'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <SignUp />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00c4cc',
+    paddingLeft: 60,
+    paddingRight: 60,
   },
 });
