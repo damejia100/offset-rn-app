5b2374fddbe0257ea69e19d48496e0944ee4026a
devAM100 <devismejia@gmail.com>
2019-11-15 18:55:11 -0500

    login and signup screens made, backend routing next

--- App.js
+++ App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SignUp from './app/components/SignUp'
-// import {StackNavigator} from 'react-navigation'
+import {
+  StyleSheet,
+  Text,
+  View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Login from './app/components/Login';
+import SignUp from './app/components/SignUp';
+import HomeScreen from './app/components/Homescreen';
 
-export default class App extends React.Component {
+// class HomeScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Home Screen</Text>
+//       </View>
+//     );
+//   }
+// }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <SignUp />
-      </View>
-    );
-  }
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#00c4cc',
-    paddingLeft: 60,
-    paddingRight: 60,
+const Application = createStackNavigator({
+  Login: {
+    screen: Login,
   },
+  SignUp: {
+    screen: SignUp
+  },
+  Home: {
+    screen: HomeScreen,
+  }
 });
+
+export default createAppContainer(Application);
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <Application />
+//     );
+//   }
+// }
+
+// <View style={styles.container}>
+      //   <SignUp />
+      // </View>
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     backgroundColor: '#00c4cc',
+//     paddingLeft: 60,
+//     paddingRight: 60,
+//   },
+// });
---
+++ app/components/Homescreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+export default class Homescreen extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Text style={styles.header}> Home </Text>
+
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00c4cc'
+  },
+  text: {
+    color: '#fff'
+  }
+})
